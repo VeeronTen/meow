04a0be7dd7108d5c62e88203eca4dfb320ec0cb0
Veeron Ten <veeronten@gmail.com>
2025-07-24 22:07:53 +0200

    now it is not scary at night

---
+++ lamp.gd
@@ -0,0 +1,61 @@
+extends Node2D
+
+signal switched
+
+@export var spring_constant: float = 100
+@export var damping_constant: float = 5
+@export var extra_distance_to_switch: float = 80
+
+@onready var original_lamp_position: Vector2 = $Lamp.position
+
+var dragging = false
+var switchedThisDrag = false
+
+func _process(delta: float) -> void:
+	queue_redraw()
+	
+func _physics_process(delta: float) -> void:
+	if dragging:
+		_applyDragForce()
+		if _lampEnoughToSwitch() and not switchedThisDrag:
+			_switch()
+		
+func _draw() -> void:
+	draw_line(Vector2.ZERO, $Lamp.position + Vector2(-4, 0) , Color(0, 0, 0), 7)
+	draw_line(Vector2.ZERO, $Lamp.position + Vector2(4, 0), Color(0, 0, 0), 7)
+	
+func _on_lamp_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
+	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
+		if event.is_pressed():
+			_startDragging()
+		else:
+			_stopDragging()
+
+func  _unhandled_input(event: InputEvent) -> void:
+	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
+		if event.is_released():
+			_stopDragging()
+
+func _lampEnoughToSwitch() -> bool:
+	var draggedDown = $Lamp.position.y - original_lamp_position.y > 0
+	var distnceFromStartEnough = $Lamp.position.distance_to(original_lamp_position) > extra_distance_to_switch
+	return draggedDown and distnceFromStartEnough
+	
+func _applyDragForce():
+	var mouse_position = get_viewport().get_mouse_position()
+	var displacement_to_mouse = mouse_position - $Lamp.global_position
+	var spring_force = displacement_to_mouse * spring_constant
+	var damping_force = -$Lamp.linear_velocity * damping_constant
+	$Lamp.apply_force(spring_force + damping_force)
+	
+func _startDragging():
+	dragging = true
+	
+func _stopDragging():
+	dragging = false
+	switchedThisDrag = false
+	
+func _switch():
+	switchedThisDrag = true
+	$SwitchSound.play()
+	switched.emit()
--- main.gd
+++ main.gd
@@ -1,5 +1,17 @@
 extends Node2D
 
+#1.  окно с птичкой, будет улетать при мягко возле нее
+#2.  кликабельный телевизор, громче при приближении
+#3.  поставка с вторым котом, после мягкой в углу
+#4.  оформление с лоадером
+#5.  рефакетор всего потому что оч много всякого в котокоде
+#6.  все расставить при изменении экрана, мяч и тд
+#7.  рандомные звуки по таймеру для эмбиента?
+#8.  звуки птички
+#9.  ночью спать скорее
+#10. иконка запуска
+#11. сраный годот спамит фейковыми изменениями (переносами строк) в *.tscn
+
 func _ready() -> void:
 	_positionEverething()
 	get_viewport().size_changed.connect(_positionEverething)
@@ -13,8 +25,8 @@ func _unhandled_input(event: InputEvent) -> void:
 	if event is InputEventMouseButton and event.is_pressed() and event.button_index == MOUSE_BUTTON_LEFT:
 		if ($ClickRayCast.clickedTo() == "Cat"):
 			$Cat.meow()
-			#todo remove
-			changeDayTime()
+		elif ($ClickRayCast.clickedTo() == "Lamp"):
+			pass
 		elif (event.position.x > $Cat.position.x):
 			$Cat.move(Vector2.RIGHT)
 		else:
@@ -44,3 +56,7 @@ func changeDayTime():
 	$NightFilter.visible = changeToNight
 	$Boomboxes/BoomboxLeft.changeMusicVolume(changeToNight) 
 	$Boomboxes/BoomboxRight.changeMusicVolume(changeToNight)	
+
+
+func _on_lamp_switched() -> void:
+	changeDayTime()
