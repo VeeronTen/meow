be10b35116e5db81c80d2d9798317a30330a2fdf
Veeron Ten <veeronten@gmail.com>
2025-07-25 21:01:55 +0200

    I DONT WATCH TV

--- entity/tv/tv.gd
+++ entity/tv/tv.gd
@@ -9,5 +9,8 @@ func _on_switch_show_timer_timeout() -> void:
 	_show()
 
 func _show():
+	var animations = $AnimationPlayer.get_animation_list()
+	var animation_to_play = animations[randi() % animations.size()]
+	$AnimationPlayer.play(animation_to_play)
 	$Screen/Show.visible = true
 	$Screen/Noise.visible = false
