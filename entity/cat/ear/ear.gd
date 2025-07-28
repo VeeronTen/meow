3af742b057f25ef5ce0732e5a78e810c9c298fc6
Veeron Ten <veeronten@gmail.com>
2025-07-28 18:59:08 +0300

    warnings + ear's refactor

--- entity/cat/ear/ear.gd
+++ entity/cat/ear/ear.gd
@@ -1,23 +1,30 @@
 class_name Ear
 extends RigidBody2D
 
-@onready var default_gravity_scale = gravity_scale
-@onready var default_linear_dampe = linear_damp
+@onready var _joint: PinJoint2D = $PinJoint2D
+@onready var _ear_down_timer: Timer = $EarsDownTimer
+@onready var _sprite: Sprite2D = $Sprite2D
+@onready var _default_gravity_scale: float = gravity_scale
+@onready var _default_linear_dampe: float = linear_damp
 
-func pin(node: Node):
-	$PinJoint2D.node_a = node.get_path()
-
-func up():
-	gravity_scale = -20.0
-	linear_damp = 20
-	$EarsDownTimer.start()
-	
-func rotateEarRight(right: bool):
-	$Sprite2D.flip_h = not right
-	
-func _earDown():
-	gravity_scale = default_gravity_scale
-	linear_damp = default_linear_dampe
-	
 func _on_ears_down_timer_timeout() -> void:
 	_earDown()
+
+
+
+func pin(node: Node) -> void:
+	_joint.node_a = node.get_path()
+
+func up() -> void:
+	gravity_scale = -20.0
+	linear_damp = 20
+	_ear_down_timer.start()
+	
+func rotateEarRight(right: bool) -> void:
+	_sprite.flip_h = not right
+
+
+
+func _earDown() -> void:
+	gravity_scale = _default_gravity_scale
+	linear_damp = _default_linear_dampe
